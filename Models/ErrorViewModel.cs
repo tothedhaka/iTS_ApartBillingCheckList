0a89c07ad0373e14b69c260f5e7ff6273d6efe6c
Md. Mofejur Rahman Babu <tothedhaka@gmail.com>
2023-09-05 10:59:10 +0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace iTS_ApartBillingCheckList.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
